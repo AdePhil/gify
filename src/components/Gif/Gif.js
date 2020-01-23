c46310388711a3437f6f5606561628dbad73aa30
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2020-01-23 17:16:20 +0100

    add description to gif

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./style.scss";
 const Gif = ({ gif }) => {
   const { title } = gif;
-  const { url } = gif.images.downsized_large;
+  const { url } = gif.images.downsized_medium;
   console.log(url);
   return (
     <div className="gif">
       <img src={url} alt={title} />
+      <div className="gif__description">
+        <p>{title}</p>
+      </div>
     </div>
   );
 };
